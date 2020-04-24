43f4c7eaafd58b222b4300bf8d6e1c343c2fcd10
Klaus <zipfel@iqo.uni-hannover.de>
2020-04-25 00:40:30 +0200

    Moved away from MP3 to OGG to reduce further lag.

--- vrguard.py
+++ vrguard.py
@@ -4,8 +4,9 @@ from pygame import mixer
 import numpy as np
 import math
 
-g_Near = "assets/271096__ianstargem__industrial-factory-fans-loop.mp3"
-g_Over = "assets/371177__samsterbirdies__alert.mp3"
+# Only use ogg of 44100 kHz, 16 Bit and stereo. mp3 will cause extra lagg, wav doesnt. But wav is unecessarily big
+g_Near = "assets/271096__ianstargem__industrial-factory-fans-loop.ogg"
+g_Over = "assets/371177__samsterbirdies__alert.ogg"
 
 class VRGuard():
     # Public members
@@ -55,7 +56,18 @@ class VRGuard():
             chaperone = vr.VRChaperone()
             _,area = chaperone.getPlayAreaRect()
             if(area.vCorners[0] != 0 and area.vCorners[1] != 0 and area.vCorners[2] != 0):
-                self.m_Bounds = area
+                # Helper vectors for easier readable code
+                v0 = area.vCorners[0]
+                v1 = area.vCorners[1]
+                v2 = area.vCorners[2]
+                v3 = area.vCorners[3]
+
+                # Push all x and z coordinates into an array...
+                x = (v0[0],v1[0],v2[0],v3[0])
+                z = (v0[2],v1[2],v2[2],v3[2])
+
+                # ... and calculate the extrema of the bounding box from it
+                self.m_Bounds = [[min(x),max(x)],[min(z),max(z)]]
             return True
         except:
             return False
@@ -63,22 +75,18 @@ class VRGuard():
     # ########### Returns the distance of a given position to the shaper m_Bounds
     def getDistanceToBounds(self, pos):
         if(not self.m_Bounds): return None
-        v0 = self.m_Bounds.vCorners[0]
-        v1 = self.m_Bounds.vCorners[1]
-        v2 = self.m_Bounds.vCorners[2]
-        v3 = self.m_Bounds.vCorners[3]
-        x = (v0[0],v1[0],v2[0],v3[0]) # x axis extrema
-        z = (v0[2],v1[2],v2[2],v3[2]) # z axis extrema
-        xMax = max(x)
-        xMin = min(x)
-        zMax = max(z)
-        zMin = min(z)
+        
+        # Helperfor easier readable code
+        xMin = self.m_Bounds[0][0]
+        xMax = self.m_Bounds[0][1]
+        zMin = self.m_Bounds[1][0]
+        zMax = self.m_Bounds[1][1]
 
         # Out of m_Bounds
-        if(pos[0] > xMax): return 0
         if(pos[0] < xMin): return 0
-        if(pos[2] > zMax): return 0
+        if(pos[0] > xMax): return 0
         if(pos[2] < zMin): return 0
+        if(pos[2] > zMax): return 0
 
         # Now calculate actual minimal distance
         delta = min(abs(np.array((xMax - pos[0], pos[0] - xMin, zMax - pos[2], pos[2] - zMin))))
@@ -113,40 +121,40 @@ class VRGuard():
     def start(self):
         self.m_Running = True
 
-        # Small buffer (512) reduces latency. Not sure, if this proposed fix of "pre_init, init, quit and init" will get rid of the ramining (annoying) lag
+        # Init audio subsystem -> Small buffer (512) reduces latency. Not sure, if this proposed fix of "pre_init, init, quit and init" will get rid of the remaining (annoying) lag
         mixer.pre_init(44100, -16, 2, 512)
         mixer.init()
         mixer.quit()
         mixer.init(44100, -16, 2, 512)
 
-        isPlaying = 0 # 0: No, 1: Near, 2: Over
+        sndCur = None # None: silent, otherwise the Sound() object, which is currently playing
+        sndOver = mixer.Sound(g_Over)
+        sndNear = mixer.Sound(g_Near)
+        
         while self.m_Running:
             if(self.isVRRunning()):
                 # Executed, when VR is running
                 dist = self.tick()
+                sndNext = None
+
                 if dist >= 0:
                     if(dist == 0):
-                        # Change audio-file and play it, if not already playing
-                        if(isPlaying != 2):
-                            isPlaying = 2
-                            mixer.music.stop()
-                            mixer.music.load(g_Over)
-                            mixer.music.play(-1)
+                        sndNext = sndOver
                     elif(dist < self.m_minDist):
-                        amp = 1 - math.exp(-3*(self.m_minDist - dist)/self.m_minDist) # Make amplitude grow exponentially from 1/e^2 to 1
-                        mixer.music.set_volume(amp)
-                        # Change audio-file and play it, if not already playing
-                        if(isPlaying != 1):
-                            isPlaying = 1
-                            mixer.music.stop()
-                            mixer.music.load(g_Near)
-                            mixer.music.play(-1)
-                    elif(isPlaying != 0):
-                        isPlaying = 0
-                        mixer.music.stop()
-                elif(isPlaying != 0):
-                    isPlaying = 0
-                    mixer.music.stop()
+                        # Make amplitude grow exponentially from 1/e^2 to 1
+                        amp = 1 - math.exp(-3*(self.m_minDist - dist)/self.m_minDist)
+                        sndNear.set_volume(amp)
+                        sndNext = sndNear
+                    elif(sndCur):
+                        sndNext = None
+                elif(sndCur):
+                    sndNext = None
+                
+                # Did the audiocontext change?
+                if(sndNext != sndCur):
+                    if(sndCur): sndCur.stop()
+                    sndCur = sndNext
+                    if(sndCur): sndCur.play(-1)
             else:
                 # VR either not running or initializing an OpenVR context failed -> Get new VR context and wait 10 seconds
                 self.startContext()
